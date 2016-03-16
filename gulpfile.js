dd7c6672b6f7aef79ee9c23f6e526e71b6fcad83
Kevin Raimbaud <kraim@free.fr>
2016-03-16 14:22:31 +0100

    Sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,31 @@
 var gulp       = require('gulp'),
     uglify     = require('gulp-uglify'),
-    uglifycss  = require('gulp-uglifycss'),
-    imagemin   = require('gulp-imagemin');
+    cssnano    = require('gulp-cssnano'),
+    imagemin   = require('gulp-imagemin'),
+    sass       = require('gulp-sass'),
+    livereload = require('gulp-livereload');
 
 gulp.task('default', function () {
-    gulp.src('assets/src/css/*.css')
-        .pipe(uglifycss())
-        .pipe(gulp.dest('assets/dist/css'));
+    gulp.src('assets/src/sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cssnano())
+        .pipe(gulp.dest('assets/dist/css'))
+        .pipe(livereload());
 
     gulp.src('assets/src/js/*.js')
         .pipe(uglify())
-        .pipe(gulp.dest('assets/dist/js'));
+        .pipe(gulp.dest('assets/dist/js'))
+        .pipe(livereload());
 
     gulp.src('assets/src/img/*.jpg')
         .pipe(imagemin())
-        .pipe(gulp.dest('assets/dist/img'));
+        .pipe(gulp.dest('assets/dist/img'))
+        .pipe(livereload());
 });
 
 gulp.task('watch', function() {
-    gulp.watch('assets/src/css/*.css', ['default']);
+    livereload.listen();
+    gulp.watch('assets/src/sass/*.scss', ['default']);
     gulp.watch('assets/src/js/*.js', ['default']);
     gulp.watch('assets/src/img/*.jpg', ['default']);
 });
