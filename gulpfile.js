0ef4426190a1dc0e88dda956a20106f14c3a769f
Kevin Raimbaud <kraim@free.fr>
2016-03-16 13:06:35 +0100

    Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,24 @@
-var gulp = require('gulp');
+var gulp       = require('gulp'),
+    uglify     = require('gulp-uglify'),
+    uglifycss  = require('gulp-uglifycss'),
+    imagemin   = require('gulp-imagemin');
 
-gulp.task('default', function(){
-    gulp.src('app.js');
+gulp.task('default', function () {
+    gulp.src('assets/src/css/*.css')
+        .pipe(uglifycss())
+        .pipe(gulp.dest('assets/dist/css'));
+
+    gulp.src('assets/src/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('assets/dist/js'));
+
+    gulp.src('assets/src/img/*.jpg')
+        .pipe(imagemin())
+        .pipe(gulp.dest('assets/dist/img'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('assets/src/css/*.css', ['default']);
+    gulp.watch('assets/src/js/*.js', ['default']);
+    gulp.watch('assets/src/img/*.jpg', ['default']);
 });
